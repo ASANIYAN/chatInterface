6f93217549c0715dd7ddb5b2d4a2e1669c7cc7da
Asaniyan Ayomide <heritageayomide2017@gmail.com>
2022-01-13 13:43:09 +0100

    styled and positioned the send button properly

--- src/InputForm.js
+++ src/InputForm.js
@@ -4,13 +4,15 @@ const InputForm = () => {
             <div className="container">
                 <div className="row">
                     <div className="col-11">
-                    <div className="input-group input-hold mb-3">
-                        <input type="text" className="form-control msgInput"
-                        placeholder="Write a message..." aria-label="Example text with button addon" aria-describedby="button-addon1" />
+                        <div className="input-group input-hold mb-3">
+                            <input type="text" className="form-control msgInput"
+                            placeholder="Write a message..." aria-label="Example text with button addon" aria-describedby="button-addon1" />
+                        </div>
                     </div>
-                    </div>
-                    <div className="col-1 pt-2">
-                        <i className="fas fa-paper-plane share"></i>
+                    <div className="col-1">
+                        <button className="btn">
+                            <i className="fas fa-paper-plane share"></i>
+                        </button>
                     </div>
                 </div>
             </div>
